c137e2a3800a1784428f9daef57ffa5a18c8e97d
gupta.anuj3 <gupta.anuj3@gmail.com>
2019-10-10 14:45:51 +0530

    New project Added Test Demo

---
+++ JenkinsTestdemo/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using JenkinsTestdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JenkinsTestdemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> stud = new List<Student>
+                 {
+                    new Student { RollNo=1, StudFirstName="Anuj", StudLastName="Gupta" },
+                    new Student { RollNo=2, StudFirstName="Jiten", StudLastName="Lad" }
+                 };
+
+            ViewData.Model = stud;
+            return View();
+        }
+    }
+}
---
+++ JenkinsTestdemo/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JenkinsTestdemo.Models
+{
+    public class Student
+    {
+        public int RollNo
+        {
+            get;
+            set;
+        }
+        public string StudFirstName
+        {
+            get;
+            set;
+        }
+        public string StudLastName
+        {
+            get;
+            set;
+        }
+    }
+}
