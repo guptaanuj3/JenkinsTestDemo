1774886dd95bd0c586266d2ddb700ff3e60553e9
gupta.anuj3 <gupta.anuj3@gmail.com>
2019-10-10 22:36:11 +0530

    List added in Controller

--- JenkinsTestdemo/Controllers/StudentController.cs
+++ JenkinsTestdemo/Controllers/StudentController.cs
@@ -15,7 +15,11 @@ namespace JenkinsTestdemo.Controllers
             List<Student> stud = new List<Student>
                  {
                     new Student { RollNo=1, StudFirstName="Anuj", StudLastName="Garg" },
-                    new Student { RollNo=2, StudFirstName="Priyanka", StudLastName="Gupta" }
+                    new Student { RollNo=2, StudFirstName="Jaymin", StudLastName="Gupta" },
+                    new Student { RollNo=1, StudFirstName="Kunal", StudLastName="Garg" },
+                    new Student { RollNo=2, StudFirstName="Rahul", StudLastName="Gupta" },
+                    new Student { RollNo=1, StudFirstName="Jiten", StudLastName="Garg" },
+                    new Student { RollNo=2, StudFirstName="Vivek", StudLastName="Gupta" }
                  };
 
             ViewData.Model = stud;
